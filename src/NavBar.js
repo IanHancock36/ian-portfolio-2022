911ca276d07503eba7a78ea14931e743d362fa89
Ian Hancock <northroadian@gmail.com>
2022-01-03 07:10:54 -0600

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import { Box, Button } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+const NavBar = () => {
+    return (
+        <div style={{}}>
+        <Box bg='#B026FF' w='100%' p={20} color='white'>
+           <Link>IH</Link> 
+        </Box>
+          </div>
+    )
+}
+
+export default NavBar
