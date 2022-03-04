d15851014102485247326225429170d15d24fa1c
Ian Hancock <northroadian@gmail.com>
2022-03-03 22:27:42 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
-
-import Navbar from '../src/Components/Navbar'
+import Home from "./Components/Home"
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar'
+import About from './Components/About'
 import  { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div >
+      <HashRouter>
       <Navbar></Navbar>
+<div>
+  <Switch>
+    <Route exact path ="/" component={Home}/>
+    <Route path="/contact"  component={Contact}/>
+    <Route path="/about"  component={About}/>
+  </Switch>
+</div>
+        </HashRouter>
+      
 
     </div>
   );
