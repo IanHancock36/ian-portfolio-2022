fd995de06846113d4298dc3aedfbf7338f4c8488
Ian Hancock <northroadian@gmail.com>
2022-03-07 06:01:28 -0600

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  <div className="navbar">
+            <h1>Navbar</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">about</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </div>
 }
 
 export default Navbar;
