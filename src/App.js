0a7b7cb123dbfafa6231558a541ea7eedb6775d6
Ian Hancock <northroadian@gmail.com>
2022-03-07 06:05:56 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Components/Home"
 import Contact from './Components/Contact';
 import Navbar from './Components/Navbar'
 import About from './Components/About'
+import Profile from './Components/Profile'
 import  {HashRouter, Switch, Route } from "react-router-dom";
 
 
@@ -16,6 +17,7 @@ function App() {
     <Route exact path ="/" component={Home}/>
     <Route path="/contact"  component={Contact}/>
     <Route path="/about"  component={About}/>
+    <Route path="/profile/:username" component={Profile}/>
   </Switch>
 </div>
         </HashRouter>
