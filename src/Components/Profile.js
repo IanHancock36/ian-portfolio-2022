6828d45c9128f1b1688a9b176a0e5a7e9378ef36
Ian Hancock <northroadian@gmail.com>
2022-03-07 06:04:18 -0600

    routes

---
+++ src/Components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from "react-router";
+
+export const Profile = () => {
+    // Use the useParams hook to get the username from the URL
+    // username has to be applied as a named parameter in the route
+    const { username } = useParams();
+
+    return (
+        <h1>{username} Profile</h1>
+    )
+}
+
+export default Profile;
